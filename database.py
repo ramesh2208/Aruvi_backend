7dc26886c0fd0857890489af3311d7b427eaaf33
Ramesh <rameshperiyasamy.n@gmail.com>
2026-03-31 12:04:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
+from dotenv import load_dotenv
 
-# Database Credentials
-USERNAME = "aruvitest"
-PASSWORD = "Ti*&#V*&urAtEst"
-HOST = "184.168.119.82"
-PORT = "3306"
-DATABASE = "aruvi_test"
+load_dotenv()
+
+# Database Credentials - Using environment variables with hardcoded fallbacks
+USERNAME = os.getenv("DB_USER", "aruvitest")
+PASSWORD = os.getenv("DB_PASSWORD", "Ti*&#V*&urAtEst")
+HOST = os.getenv("DB_HOST", "184.168.119.82")
+PORT = os.getenv("DB_PORT", "3306")
+DATABASE = os.getenv("DB_NAME", "aruvi_test")
 
 encoded_password = quote_plus(PASSWORD)
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USERNAME}:{encoded_password}@{HOST}:{PORT}/{DATABASE}"
